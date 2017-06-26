7766184d340d1177c4c74acb25e683dabbe1c2f4
HC <hc_liu@gemteks.com>
2017-06-26 16:19:26 +0800

    Add files via upload
    update setup.sh for correct installation

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 echo "installing system required packages";
-sudo apt-get install -y mosquitto python-pip;
-sudo pip install paho-mqtt python-etcd pyserial;
+apt-get install -y mosquitto python-pip;
+pip install paho-mqtt python-etcd pyserial;
 
 echo "installing lora_repeater package";
-git clone https://github.com/hc-liu/lora_repeater.git
+git clone https://github.com/hc-liu/lora_repeater.git;
 cd lora_repeater/ ; 
 cp inq.py deq.py /usr/local/bin/;
-sudo cp lora_repeater_starter /etc/init.d/;
-sudo chmod 755 /usr/local/bin/inq.py /usr/local/bin/deq.py /etc/init.d/lora_repeater_starter;
+cp lora_repeater_starter /etc/init.d/;
+chmod 755 /usr/local/bin/inq.py /usr/local/bin/deq.py /etc/init.d/lora_repeater_starter;
 mv /usr/local/bin/inq.py /usr/local/bin/inq; mv /usr/local/bin/deq.py /usr/local/bin/deq;
 update-rc.d lora_repeater_starter remove;
 update-rc.d lora_repeater_starter defaults;
