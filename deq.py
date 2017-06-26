c7d9da17d1bdc236b764f1c758d7e065c6a3474f
HC <hc_liu@gemteks.com>
2017-06-26 14:12:05 +0800

    Add files via upload
    Add logging function

--- deq.py
+++ deq.py
@@ -5,18 +5,22 @@ import json
 import sys        
 import time
 import serial
+import logging
+from logging.handlers import RotatingFileHandler
           
 MY_SENDING_NODE_DEV0_PATH="/dev/ttyUSB0" 
 MY_SENDING_NODE_DEV1_PATH="/dev/ttyUSB1" 
 
 MY_SLEEP_INTERVAL=3
-MY_ALIVE_INTERVAL=30
+MY_ALIVE_INTERVAL=60
    
 MY_MQTT_QUEUE_FILE_PATH="/var/lora_repeater/queue/"
 MY_SENDING_FILE_PATH="/var/lora_repeater/sending/"
 MY_SENT_FILE_PATH="/var/lora_repeater/sent/"
 MY_SEND_FAIL_FILE_PATH="/var/lora_repeater/fail/"
 MY_LOG_FILE_PATH="/var/lora_repeater/log/"
+
+MY_LOG_FILENAME = MY_LOG_FILE_PATH+"deq.log"
 
 MY_NODE_MAC_ADDR=""
 MY_NODE_MAC_ADDR_SHORT=""
@@ -33,7 +37,27 @@ def check_lora_module(dev_path):
 		#print 'FAIL: Cannot open Serial Port (No LoRa Node Inserted)'
 		return None
 
-#start:    
+#start:   
+
+# Set up a specific logger with our desired output level
+my_logger = logging.getLogger('dequeue')
+# Add the log message handler to the logger
+my_logger.setLevel(logging.DEBUG)
+handler = RotatingFileHandler(MY_LOG_FILENAME, maxBytes=10240, backupCount=100)
+# create a logging format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+my_logger.addHandler(handler)
+my_logger.info('I am started!')
+
+#example
+#my_logger.debug('debug message')
+#my_logger.info('info message')
+#my_logger.warn('warn message')
+#my_logger.error('error message')
+#my_logger.critical('critical message')
+
+ 
 #make queue file folder
 if not os.path.exists(MY_MQTT_QUEUE_FILE_PATH):
 	os.makedirs(MY_MQTT_QUEUE_FILE_PATH)
@@ -54,37 +78,47 @@ ser = check_lora_module(MY_SENDING_NODE_DEV0_PATH)
 if ser is None:
 	ser = check_lora_module(MY_SENDING_NODE_DEV1_PATH)
 	if ser is None:
-		print 'FAIL to connect LoRa node!'
+		#print 'FAIL to connect LoRa node!'
+		my_logger.error('FAIL to connect LoRa node!')
 		sys.exit()
 	else:
-		print("Open LoRa node done:" + MY_SENDING_NODE_DEV1_PATH)   
+		#print("Open LoRa node done:" + MY_SENDING_NODE_DEV1_PATH)   
+		my_logger.info('Open LoRa node done: /dev/ttyUSB0')
 else:   
-	print("Open LoRa node done:" + MY_SENDING_NODE_DEV0_PATH)   
+	#print("Open LoRa node done:" + MY_SENDING_NODE_DEV0_PATH)  
+	my_logger.info('Open LoRa node done: /dev/ttyUSB1')
 
 #queue my Lora node mac address AT+SGMD?, return +SGMD:"040004C5","GLN0161400362"
 ser.flushInput()
 ser.flushOutput()
 ser.write("AT+SGMD?\n")
+my_logger.info('Write AT+SGMD?')
 return_state=ser.readlines()
+my_logger.info(return_state)
 #print return_state
 
 matching = [s for s in return_state if "+SGMD:" in s]
 #print matching  #['+SGMD:"05000095","GLN0154700000"\r\n']
 MY_NODE_MAC_ADDR_SHORT = matching[0][7:15]
 MY_NODE_MAC_ADDR = "00000000"+ MY_NODE_MAC_ADDR_SHORT
-print ("Check: My LoRa Node MAC Addr:" + MY_NODE_MAC_ADDR)
+#print ("Check: My LoRa Node MAC Addr:" + MY_NODE_MAC_ADDR)
+my_logger.info('Check: My LoRa Node MAC Addr:')
+my_logger.info(MY_NODE_MAC_ADDR)
+
 
 while 1:  
 	for sending_f in os.listdir(MY_MQTT_QUEUE_FILE_PATH):
-		print sending_f
-
+		#print sending_f
+		my_logger.info(sending_f)
 		#read mqtt payload its mine
 		if MY_NODE_MAC_ADDR in sending_f:
-			print("#############Sending by Myself#########")
+			#print("#############Sending by Myself#########")
+			my_logger.info('#############Sending by Myself#########')
 			os.remove(MY_MQTT_QUEUE_FILE_PATH+sending_f)
 			continue
 		else:
-			print("Prepare to send data")
+			#print("Prepare to send data")
+			my_logger.info('Prepare to send data')
 				
 		os.rename(MY_MQTT_QUEUE_FILE_PATH+sending_f, MY_SENDING_FILE_PATH+sending_f)
 		f = open(MY_SENDING_FILE_PATH+sending_f, 'r')
@@ -94,27 +128,29 @@ while 1:
 		sensor_data_len = len(sensor_data)
 		#print("sensor_data:" + sensor_data )
 		data_sending = "AT+DTX="+str(sensor_data_len)+","+sensor_data+"\n"
-		print(data_sending)
-		#return_state=ser.readlines()
-		#print(return_state)
 		time.sleep(MY_SLEEP_INTERVAL)
 		GLOBAL_TIME_RUNNING+=MY_SLEEP_INTERVAL
-		print('Sending')
+		#print('Sending')
+		my_logger.info('Sending')
 		ser.flushInput()
 		ser.flushOutput()
 		ser.write(data_sending)
+		my_logger.info(data_sending)
 		return_state=ser.readlines()
-		print(return_state)
+		#print(return_state)
+		my_logger.info(return_state)
 		if "OK\r\n" in return_state:
-			print("Result: SENT.")
+			#print("Result: SENT.")
+			my_logger.info('Result: SENT.')
 			GLOBAL_COUNT_SENT+=1
 			os.rename(MY_SENDING_FILE_PATH+sending_f, MY_SENT_FILE_PATH+sending_f)
 		else:
-			print("Result: FAIL! move to FAIL")
+			#print("Result: FAIL! move to FAIL")
+			my_logger.error('Result: FAIL! move to FAIL.')
 			GLOBAL_COUNT_FAIL+=1
 			os.rename(MY_SENDING_FILE_PATH+sending_f, MY_SEND_FAIL_FILE_PATH+sending_f)
 	else:
-		print("Waiting for incoming queue")
+		#print("Waiting for incoming queue")
 		time.sleep(MY_SLEEP_INTERVAL)
 		GLOBAL_TIME_RUNNING+=MY_SLEEP_INTERVAL
 	
@@ -126,9 +162,12 @@ while 1:
 		data_alive_send = "AT+DTX=" + str (len(data_alive)) + "," + data_alive + "\n"
 		ser.flushInput()
 		ser.flushOutput()
+		my_logger.info(data_alive_send)
 		ser.write(data_alive_send)
 		return_state=ser.readlines()
-		print(return_state)
+		my_logger.info('Alive message')
+		my_logger.info(return_state)
+		#print(return_state)
 		GLOBAL_COUNT_SENT=0
 		GLOBAL_COUNT_FAIL=0
 		GLOBAL_TIME_RUNNING=0
--- inq.py
+++ inq.py
@@ -4,6 +4,8 @@ import paho.mqtt.client as mqtt
 import json
 import sys
 import os 
+import logging
+from logging.handlers import RotatingFileHandler
 
 MY_MQTT_QUEUE_FILE_PATH="/var/lora_repeater/queue/"
 MY_SENDING_FILE_PATH="/var/lora_repeater/sending/"
@@ -11,29 +13,51 @@ MY_SENT_FILE_PATH="/var/lora_repeater/sent/"
 MY_SEND_FAIL_FILE_PATH="/var/lora_repeater/fail/"
 MY_LOG_FILE_PATH="/var/lora_repeater/log/"
 
+MY_LOG_FILENAME = MY_LOG_FILE_PATH+"inq.log"
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    #client.subscribe("$SYS/#")
-    client.subscribe("#")
+    #print("Connected with result code "+str(rc))
+	my_logger.info('Mqtt Connected.')
+	client.subscribe("#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    #print(msg.topic+" "+str(msg.payload))
+    my_logger.info('Message incoming')
+    #my_logger.info(msg.topic)
+    #my_logger.info(msg.payload) log will fail
     json_data = msg.payload
     sensor_mac = json.loads(json_data)[0]['macAddr']
     sensor_data = json.loads(json_data)[0]['data']
     sensor_count = json.loads(json_data)[0]['frameCnt']
     
+    my_logger.info('Data is:')
+    my_logger.info(sensor_data)
     f = open(MY_MQTT_QUEUE_FILE_PATH+sensor_mac+"-"+str(sensor_count), 'w')
     f.write(json_data)
     f.close
-    print('data = ' + sensor_data)
+    #print('data = ' + sensor_data)
  
-#start:    
+#start:  
+# Set up a specific logger with our desired output level
+my_logger = logging.getLogger('enqueue')
+# Add the log message handler to the logger
+my_logger.setLevel(logging.DEBUG)
+handler = RotatingFileHandler(MY_LOG_FILENAME, maxBytes=10240, backupCount=100)
+# create a logging format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+my_logger.addHandler(handler)
+my_logger.info('I am started!')
+#example
+#my_logger.debug('debug message')
+#my_logger.info('info message')
+#my_logger.warn('warn message')
+#my_logger.error('error message')
+#my_logger.critical('critical message')
+
+  
 #make queue file folder
 if not os.path.exists(MY_MQTT_QUEUE_FILE_PATH):
     os.makedirs(MY_MQTT_QUEUE_FILE_PATH)
