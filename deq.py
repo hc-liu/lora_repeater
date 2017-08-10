2283295eec0221cfb0582d433fea6924f4800b21
hc_liu <hc_liu@gemteks.com>
2017-08-10 15:46:55 +0800

    reformat the code for tab and space mixed issue

--- deq.py
+++ deq.py
@@ -1,61 +1,62 @@
 #!/usr/bin/python
-          
-import os  
+
+import os
 import json
-import sys        
+import sys
 import time
 import serial
 import logging
 from logging.handlers import RotatingFileHandler
-          
-MY_SENDING_NODE_DEV0_PATH="/dev/ttyUSB0" 
-MY_SENDING_NODE_DEV1_PATH="/dev/ttyUSB1" 
 
-MY_SLEEP_INTERVAL=3
-MY_ALIVE_INTERVAL=60
-   
-MY_MQTT_QUEUE_FILE_PATH="/var/lora_repeater/queue/"
-MY_SENDING_FILE_PATH="/var/lora_repeater/sending/"
-MY_SENT_FILE_PATH="/var/lora_repeater/sent/"
-MY_SEND_FAIL_FILE_PATH="/var/lora_repeater/fail/"
-MY_LOG_FILE_PATH="/var/lora_repeater/log/"
+MY_SENDING_NODE_DEV0_PATH = "/dev/ttyUSB0"
+MY_SENDING_NODE_DEV1_PATH = "/dev/ttyUSB1"
 
-MY_LOG_FILENAME = MY_LOG_FILE_PATH+"deq.log"
+MY_SLEEP_INTERVAL = 3
+MY_ALIVE_INTERVAL = 60
 
-MY_NODE_MAC_ADDR=""
-MY_NODE_MAC_ADDR_SHORT=""
+MY_MQTT_QUEUE_FILE_PATH = "/var/lora_repeater/queue/"
+MY_SENDING_FILE_PATH = "/var/lora_repeater/sending/"
+MY_SENT_FILE_PATH = "/var/lora_repeater/sent/"
+MY_SEND_FAIL_FILE_PATH = "/var/lora_repeater/fail/"
+MY_LOG_FILE_PATH = "/var/lora_repeater/log/"
 
-GLOBAL_TIME_RUNNING=0
-GLOBAL_COUNT_SENT=0
-GLOBAL_COUNT_FAIL=0
+MY_LOG_FILENAME = MY_LOG_FILE_PATH + "deq.log"
 
-SENT_OK_TAG="Radio Tx Done\r\n"
+MY_NODE_MAC_ADDR = ""
+MY_NODE_MAC_ADDR_SHORT = ""
+
+GLOBAL_TIME_RUNNING = 0
+GLOBAL_COUNT_SENT = 0
+GLOBAL_COUNT_FAIL = 0
+
+SENT_OK_TAG = "Radio Tx Done\r\n"
 
 
 def check_lora_module(dev_path):
-	try:
-		ser = serial.Serial(dev_path, 9600, timeout=0.5)
-		return ser
-	except serial.serialutil.SerialException:
-		#print 'FAIL: Cannot open Serial Port (No LoRa Node Inserted)'
-		return None
+    try:
+        ser = serial.Serial(dev_path, 9600, timeout=0.5)
+        return ser
+    except serial.serialutil.SerialException:
+        # print 'FAIL: Cannot open Serial Port (No LoRa Node Inserted)'
+        return None
 
-#start:   
-#make queue file folder
+
+# start:
+# make queue file folder
 if not os.path.exists(MY_MQTT_QUEUE_FILE_PATH):
-	os.makedirs(MY_MQTT_QUEUE_FILE_PATH)
-#make sending file folder    
+    os.makedirs(MY_MQTT_QUEUE_FILE_PATH)
+# make sending file folder
 if not os.path.exists(MY_SENDING_FILE_PATH):
-	os.makedirs(MY_SENDING_FILE_PATH)
-#make sent file folder    
+    os.makedirs(MY_SENDING_FILE_PATH)
+# make sent file folder
 if not os.path.exists(MY_SENT_FILE_PATH):
-	os.makedirs(MY_SENT_FILE_PATH)
-#make sending fail file folder    
+    os.makedirs(MY_SENT_FILE_PATH)
+# make sending fail file folder
 if not os.path.exists(MY_SEND_FAIL_FILE_PATH):
-	os.makedirs(MY_SEND_FAIL_FILE_PATH)            
-#make log file folder
+    os.makedirs(MY_SEND_FAIL_FILE_PATH)
+# make log file folder
 if not os.path.exists(MY_LOG_FILE_PATH):
-	os.makedirs(MY_LOG_FILE_PATH)
+    os.makedirs(MY_LOG_FILE_PATH)
 
 # Set up a specific logger with our desired output level
 my_logger = logging.getLogger('dequeue')
@@ -68,38 +69,38 @@ handler.setFormatter(formatter)
 my_logger.addHandler(handler)
 my_logger.info('I am started!')
 
-#example
-#my_logger.debug('debug message')
-#my_logger.info('info message')
-#my_logger.warn('warn message')
-#my_logger.error('error message')
-#my_logger.critical('critical message')
+# example
+# my_logger.debug('debug message')
+# my_logger.info('info message')
+# my_logger.warn('warn message')
+# my_logger.error('error message')
+# my_logger.critical('critical message')
 
 ser = check_lora_module(MY_SENDING_NODE_DEV0_PATH)
 if ser is None:
-	ser = check_lora_module(MY_SENDING_NODE_DEV1_PATH)
-	if ser is None:
-		#print 'FAIL to connect LoRa node!'
-		my_logger.error('FAIL to connect LoRa node!')
-		sys.exit()
-	else:
-		#print("Open LoRa node done:" + MY_SENDING_NODE_DEV1_PATH)   
-		my_logger.info('Open LoRa node done: /dev/ttyUSB0')
-else:   
-	#print("Open LoRa node done:" + MY_SENDING_NODE_DEV0_PATH)  
-	my_logger.info('Open LoRa node done: /dev/ttyUSB1')
+    ser = check_lora_module(MY_SENDING_NODE_DEV1_PATH)
+    if ser is None:
+        # print 'FAIL to connect LoRa node!'
+        my_logger.error('FAIL to connect LoRa node!')
+        sys.exit()
+    else:
+        # print("Open LoRa node done:" + MY_SENDING_NODE_DEV1_PATH)
+        my_logger.info('Open LoRa node done: /dev/ttyUSB0')
+else:
+    # print("Open LoRa node done:" + MY_SENDING_NODE_DEV0_PATH)
+    my_logger.info('Open LoRa node done: /dev/ttyUSB1')
 
-#queue my Lora node mac address AT+SGMD?, return +SGMD:"040004C5","GLN0161400362"
+# queue my Lora node mac address AT+SGMD?, return +SGMD:"040004C5","GLN0161400362"
 ser.flushInput()
 ser.flushOutput()
 # time.sleep(MY_SLEEP_INTERVAL)
 # #ser.write("AT+SGMD?\n")
 # ser.write("AT+CDEVADDR?\n")
 # my_logger.info('Write AT+CDEVADDR?')
-return_state=ser.readlines()
-#time.sleep(MY_SLEEP_INTERVAL)
-#my_logger.info(return_state)
-#print return_state
+return_state = ser.readlines()
+# time.sleep(MY_SLEEP_INTERVAL)
+# my_logger.info(return_state)
+# print return_state
 
 # if not return_state:
 # 	my_logger.error('return of AT+CDEVADDR? is empty!, restart!')
@@ -110,8 +111,8 @@ return_state=ser.readlines()
 # 	my_logger.info(return_state)
 # 
 # matching = [s for s in return_state if "+CDEVADDR:" in s]
-#print matching  #['+SGMD:"05000095","GLN0154700000"\r\n']
-#MY_NODE_MAC_ADDR_SHORT = matching[0][10:18]
+# print matching  #['+SGMD:"05000095","GLN0154700000"\r\n']
+# MY_NODE_MAC_ADDR_SHORT = matching[0][10:18]
 # starPosition = matching[0].index("+CDEVADDR:")
 # #print starPosition
 # starPosition = starPosition+len("+CDEVADDR:")
@@ -125,125 +126,126 @@ return_state=ser.readlines()
 
 my_dict = {}
 
-while 1:  
-	for sending_f in os.listdir(MY_MQTT_QUEUE_FILE_PATH):
-		#print sending_f
-		my_logger.info(sending_f)
-		#read mqtt payload its mine
-		# if MY_NODE_MAC_ADDR in sending_f:
-# 			#print("#############Sending by Myself#########")
-# 			my_logger.info('#############Sending by Myself#########')
-# 			os.remove(MY_MQTT_QUEUE_FILE_PATH+sending_f)
-# 			continue
-# 		else:
-# 			#print("Prepare to send data")
-# 			my_logger.info('Prepare to send data')
-# 		
-		#protection
-		os.rename(MY_MQTT_QUEUE_FILE_PATH+sending_f, MY_SENDING_FILE_PATH+sending_f)
-		my_logger.info('Rename File Done')
-		f = open(MY_SENDING_FILE_PATH+sending_f, 'r')
-		my_logger.info('Open File Done')
-		f_json_data = f.read()
-		my_logger.info('Read File Done')
-		f.close
-		my_logger.info('Close File Done')
-		sensor_data = str(json.loads(f_json_data)[0]['data'])
-		sensor_macAddr = str(json.loads(f_json_data)[0]['macAddr'])
-		sensor_frameCnt = str(json.loads(f_json_data)[0]['frameCnt'])
-		nFrameCnt = json.loads(f_json_data)[0]['frameCnt']
-		#print my_dict
-		bSending = True;
-		if sensor_macAddr in my_dict:
-			nPrevFrameCnt = my_dict.get(sensor_macAddr)
-			if nFrameCnt == 1:
-				my_dict[sensor_macAddr] = 1
-				bSending = False
-			elif nFrameCnt == nPrevFrameCnt:
-				bSending = False
-			elif nPrevFrameCnt-nFrameCnt > 10:
-				my_dict[sensor_macAddr] = nFrameCnt
-				bSending = False
-			elif nFrameCnt > nPrevFrameCnt:
-				my_dict[sensor_macAddr] = nFrameCnt
-			else:
-				bSending = False
-			#print 'exist=' + sensor_macAddr
-		else:
-			my_dict[sensor_macAddr] = nFrameCnt
-			#print 'add=' + sensor_macAddr
-			if nFrameCnt == 1:
-				bSending = False
-		#print my_dict
-		if bSending is False:
-			my_logger.info('this package Can NOT be sent!')
-			os.remove(MY_SENDING_FILE_PATH+sending_f)
-			continue
-		else:
-			my_logger.info('this package Will be sent!')
-		
-		my_logger.info('macAddr')
-		my_logger.info(sensor_macAddr[8:16])
-		sensor_nwkskey=""
-		sensor_appskey=""
-		#print(sensor_macAddr[8:10])
-		if "04" in sensor_macAddr[8:10]:
-			sensor_nwkskey = "43610A1F04719BB807A8073F8AECB131"
-			sensor_appskey = sensor_nwkskey
-		
-		if "05" in sensor_macAddr[8:10]:
-			sensor_nwkskey = "63D83C7F054A18D423BDFB712D8F4371"
-			sensor_appskey = sensor_nwkskey
-			
-		if "0d" in sensor_macAddr[8:10]:
-			sensor_nwkskey = "933C5ACB0D941ED1EC32DA7D9174C452"
-			sensor_appskey = sensor_nwkskey
-				
-		my_logger.info('frameCnt');
-		my_logger.info(sensor_frameCnt);
-		my_logger.info('DATA')
-		my_logger.info(sensor_data)
-		sensor_data_len = len(sensor_data)
-		my_logger.info('DATA Len')
-		my_logger.info(sensor_data_len)
-	
-		#print("sensor_data:" + sensor_data )
-		#data_sending = "AT+DTX="+str(sensor_data_len)+","+sensor_data+"\n"
-		data_sending = "AT+SSTX="+str(sensor_data_len)+","+sensor_data+","+sensor_macAddr[8:16]+","+sensor_frameCnt+","+sensor_nwkskey+","+sensor_appskey+"\n"
-		my_logger.info(data_sending)
-		#print data_sending
-		time.sleep(MY_SLEEP_INTERVAL)
-		GLOBAL_TIME_RUNNING+=MY_SLEEP_INTERVAL
-		#print('GLOBAL_TIME_RUNNING')
-		#print(GLOBAL_TIME_RUNNING)
-		my_logger.info('Sending')
-		ser.flushInput()
-		ser.flushOutput()
-		ser.write(data_sending)
-		my_logger.info(data_sending)
-		return_state=ser.readlines()
-		#print(return_state)
-		my_logger.info(return_state)
-		if SENT_OK_TAG in return_state:
-			#print("Result: SENT.")
-			my_logger.info('Result: SENT.')
-			GLOBAL_COUNT_SENT+=1
-			os.rename(MY_SENDING_FILE_PATH+sending_f, MY_SENT_FILE_PATH+sending_f)
-		else:
-			#print("Result: FAIL! move to FAIL")
-			my_logger.error('Result: FAIL! move to FAIL.')
-			GLOBAL_COUNT_FAIL+=1
-			os.rename(MY_SENDING_FILE_PATH+sending_f, MY_SEND_FAIL_FILE_PATH+sending_f)
-	else:
-		#print("Waiting for incoming queue")
-		time.sleep(MY_SLEEP_INTERVAL)
-	#	GLOBAL_TIME_RUNNING+=MY_SLEEP_INTERVAL
-	
-	#print("in while loop")
-# 	if GLOBAL_TIME_RUNNING >= MY_ALIVE_INTERVAL:
-		#send alive command
-	# 	time.sleep(MY_SLEEP_INTERVAL)
-# 		data_alive = MY_NODE_MAC_ADDR_SHORT + "E" +str(GLOBAL_COUNT_SENT) + "F" +str(GLOBAL_COUNT_FAIL)
+while 1:
+    for sending_f in os.listdir(MY_MQTT_QUEUE_FILE_PATH):
+        # print sending_f
+        my_logger.info(sending_f)
+        # read mqtt payload its mine
+        # if MY_NODE_MAC_ADDR in sending_f:
+        # 			#print("#############Sending by Myself#########")
+        # 			my_logger.info('#############Sending by Myself#########')
+        # 			os.remove(MY_MQTT_QUEUE_FILE_PATH+sending_f)
+        # 			continue
+        # 		else:
+        # 			#print("Prepare to send data")
+        # 			my_logger.info('Prepare to send data')
+        #
+        # protection
+        os.rename(MY_MQTT_QUEUE_FILE_PATH + sending_f, MY_SENDING_FILE_PATH + sending_f)
+        my_logger.info('Rename File Done')
+        f = open(MY_SENDING_FILE_PATH + sending_f, 'r')
+        my_logger.info('Open File Done')
+        f_json_data = f.read()
+        my_logger.info('Read File Done')
+        f.close
+        my_logger.info('Close File Done')
+        sensor_data = str(json.loads(f_json_data)[0]['data'])
+        sensor_macAddr = str(json.loads(f_json_data)[0]['macAddr'])
+        sensor_frameCnt = str(json.loads(f_json_data)[0]['frameCnt'])
+        nFrameCnt = json.loads(f_json_data)[0]['frameCnt']
+        # print my_dict
+        bSending = True;
+        if sensor_macAddr in my_dict:
+            nPrevFrameCnt = my_dict.get(sensor_macAddr)
+            if nFrameCnt == 1:
+                my_dict[sensor_macAddr] = 1
+                bSending = False
+            elif nFrameCnt == nPrevFrameCnt:
+                bSending = False
+            elif nPrevFrameCnt - nFrameCnt > 10:
+                my_dict[sensor_macAddr] = nFrameCnt
+                bSending = False
+            elif nFrameCnt > nPrevFrameCnt:
+                my_dict[sensor_macAddr] = nFrameCnt
+            else:
+                bSending = False
+            # print 'exist=' + sensor_macAddr
+        else:
+            my_dict[sensor_macAddr] = nFrameCnt
+            # print 'add=' + sensor_macAddr
+            if nFrameCnt == 1:
+                bSending = False
+        # print my_dict
+        if bSending is False:
+            my_logger.info('this package Can NOT be sent!')
+            os.remove(MY_SENDING_FILE_PATH + sending_f)
+            continue
+        else:
+            my_logger.info('this package Will be sent!')
+
+        my_logger.info('macAddr')
+        my_logger.info(sensor_macAddr[8:16])
+        sensor_nwkskey = ""
+        sensor_appskey = ""
+        # print(sensor_macAddr[8:10])
+        if "04" in sensor_macAddr[8:10]:
+            sensor_nwkskey = "43610A1F04719BB807A8073F8AECB131"
+            sensor_appskey = sensor_nwkskey
+
+        if "05" in sensor_macAddr[8:10]:
+            sensor_nwkskey = "63D83C7F054A18D423BDFB712D8F4371"
+            sensor_appskey = sensor_nwkskey
+
+        if "0d" in sensor_macAddr[8:10]:
+            sensor_nwkskey = "933C5ACB0D941ED1EC32DA7D9174C452"
+            sensor_appskey = sensor_nwkskey
+
+        my_logger.info('frameCnt');
+        my_logger.info(sensor_frameCnt);
+        my_logger.info('DATA')
+        my_logger.info(sensor_data)
+        sensor_data_len = len(sensor_data)
+        my_logger.info('DATA Len')
+        my_logger.info(sensor_data_len)
+
+        # print("sensor_data:" + sensor_data )
+        # data_sending = "AT+DTX="+str(sensor_data_len)+","+sensor_data+"\n"
+        data_sending = "AT+SSTX=" + str(sensor_data_len) + "," + sensor_data + "," + sensor_macAddr[
+                                                                                     8:16] + "," + sensor_frameCnt + "," + sensor_nwkskey + "," + sensor_appskey + "\n"
+        my_logger.info(data_sending)
+        # print data_sending
+        time.sleep(MY_SLEEP_INTERVAL)
+        GLOBAL_TIME_RUNNING += MY_SLEEP_INTERVAL
+        # print('GLOBAL_TIME_RUNNING')
+        # print(GLOBAL_TIME_RUNNING)
+        my_logger.info('Sending')
+        ser.flushInput()
+        ser.flushOutput()
+        ser.write(data_sending)
+        my_logger.info(data_sending)
+        return_state = ser.readlines()
+        # print(return_state)
+        my_logger.info(return_state)
+        if SENT_OK_TAG in return_state:
+            # print("Result: SENT.")
+            my_logger.info('Result: SENT.')
+            GLOBAL_COUNT_SENT += 1
+            os.rename(MY_SENDING_FILE_PATH + sending_f, MY_SENT_FILE_PATH + sending_f)
+        else:
+            # print("Result: FAIL! move to FAIL")
+            my_logger.error('Result: FAIL! move to FAIL.')
+            GLOBAL_COUNT_FAIL += 1
+            os.rename(MY_SENDING_FILE_PATH + sending_f, MY_SEND_FAIL_FILE_PATH + sending_f)
+    else:
+        # print("Waiting for incoming queue")
+        time.sleep(MY_SLEEP_INTERVAL)
+    #	GLOBAL_TIME_RUNNING+=MY_SLEEP_INTERVAL
+
+    # print("in while loop")
+    # 	if GLOBAL_TIME_RUNNING >= MY_ALIVE_INTERVAL:
+    # send alive command
+    # 	time.sleep(MY_SLEEP_INTERVAL)
+# data_alive = MY_NODE_MAC_ADDR_SHORT + "E" +str(GLOBAL_COUNT_SENT) + "F" +str(GLOBAL_COUNT_FAIL)
 # 		data_alive_send = "AT+DTX=" + str (len(data_alive)) + "," + data_alive + "\n"
 # 		ser.flushInput()
 # 		ser.flushOutput()
@@ -257,6 +259,3 @@ while 1:
 # 		GLOBAL_COUNT_FAIL=0
 # 		GLOBAL_TIME_RUNNING=0
 ser.close
-   
-
- 
--- inq.py
+++ inq.py
@@ -3,95 +3,98 @@
 import paho.mqtt.client as mqtt
 import json
 import sys
-import os 
+import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-MY_MQTT_QUEUE_FILE_PATH="/var/lora_repeater/queue/"
-MY_SENDING_FILE_PATH="/var/lora_repeater/sending/"
-MY_SENT_FILE_PATH="/var/lora_repeater/sent/"
-MY_SEND_FAIL_FILE_PATH="/var/lora_repeater/fail/"
-MY_LOG_FILE_PATH="/var/lora_repeater/log/"
+MY_MQTT_QUEUE_FILE_PATH = "/var/lora_repeater/queue/"
+MY_SENDING_FILE_PATH = "/var/lora_repeater/sending/"
+MY_SENT_FILE_PATH = "/var/lora_repeater/sent/"
+MY_SEND_FAIL_FILE_PATH = "/var/lora_repeater/fail/"
+MY_LOG_FILE_PATH = "/var/lora_repeater/log/"
 
-MY_LOG_FILENAME = MY_LOG_FILE_PATH+"inq.log"
+MY_LOG_FILENAME = MY_LOG_FILE_PATH + "inq.log"
 
 my_dict = {}
 
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    #print("Connected with result code "+str(rc))
-	my_logger.info('Mqtt Connected.')
-	client.subscribe("#")
+    # print("Connected with result code "+str(rc))
+    my_logger.info('Mqtt Connected.')
+    client.subscribe("#")
+
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     try:
-    	#print(msg.topic+" "+str(msg.payload))
-    	my_logger.info('Message incoming')
-    	my_logger.info(msg.topic)
-    	my_logger.info(msg.payload) 
-    	json_data = msg.payload
-    	sensor_mac = json.loads(json_data)[0]['macAddr']
-    	sensor_data = json.loads(json_data)[0]['data']
-    	sensor_count = json.loads(json_data)[0]['frameCnt']
-    	nFrameCnt = json.loads(json_data)[0]['frameCnt']
-    	
-    	my_logger.info('Data is:')
-    	my_logger.info(sensor_data)
-    	
-    	my_logger.info('now frameCnt is:')
-    	my_logger.info(sensor_count)
-    	
-    	bQueue = True
-    	if sensor_mac in my_dict:
-    		nPrevFrameCnt = my_dict.get(sensor_mac)
-    		my_logger.info('prev frameCnt is:')
-    		my_logger.info(nPrevFrameCnt)
-    		
-    		if nFrameCnt == 1:
-    			my_dict[sensor_mac] = 1
-    			bQueue = True
-    		elif nFrameCnt == nPrevFrameCnt:
-    			bQueue = False
-    		elif nFrameCnt < 10:
-    			bQueue = True
-    			my_dict[sensor_mac] = nFrameCnt
-    		elif nFrameCnt-nPrevFrameCnt > 10:
-    			bQueue = False
-    		elif nFrameCnt > nPrevFrameCnt:
-    			my_dict[sensor_mac] = nFrameCnt
-    			bQueue = True
-    		else:
-    			bQueue = False
-    	else:
-    		my_dict[sensor_mac] = nFrameCnt
-    		bQueue = True
-    		
-    	if bQueue is False:
-    		my_logger.info('this package Can NOT be queue!')
-    	else:
-    		my_logger.info('this package Will be queue!')
-    		f = open(MY_MQTT_QUEUE_FILE_PATH+sensor_mac+"-"+str(sensor_count), 'w')
-    		f.write(json_data)
-    		f.close
-    		#print('data = ' + sensor_data)
+        # print(msg.topic+" "+str(msg.payload))
+        my_logger.info('Message incoming')
+        my_logger.info(msg.topic)
+        my_logger.info(msg.payload)
+        json_data = msg.payload
+        sensor_mac = json.loads(json_data)[0]['macAddr']
+        sensor_data = json.loads(json_data)[0]['data']
+        sensor_count = json.loads(json_data)[0]['frameCnt']
+        nFrameCnt = json.loads(json_data)[0]['frameCnt']
+
+        my_logger.info('Data is:')
+        my_logger.info(sensor_data)
+
+        my_logger.info('now frameCnt is:')
+        my_logger.info(sensor_count)
+
+        bQueue = True
+        if sensor_mac in my_dict:
+            nPrevFrameCnt = my_dict.get(sensor_mac)
+            my_logger.info('prev frameCnt is:')
+            my_logger.info(nPrevFrameCnt)
+
+            if nFrameCnt == 1:
+                my_dict[sensor_mac] = 1
+                bQueue = True
+            elif nFrameCnt == nPrevFrameCnt:
+                bQueue = False
+            elif nFrameCnt < 10:
+                bQueue = True
+                my_dict[sensor_mac] = nFrameCnt
+            elif nFrameCnt - nPrevFrameCnt > 10:
+                bQueue = False
+            elif nFrameCnt > nPrevFrameCnt:
+                my_dict[sensor_mac] = nFrameCnt
+                bQueue = True
+            else:
+                bQueue = False
+        else:
+            my_dict[sensor_mac] = nFrameCnt
+            bQueue = True
+
+        if bQueue is False:
+            my_logger.info('this package Can NOT be queue!')
+        else:
+            my_logger.info('this package Will be queue!')
+            f = open(MY_MQTT_QUEUE_FILE_PATH + sensor_mac + "-" + str(sensor_count), 'w')
+            f.write(json_data)
+            f.close
+        # print('data = ' + sensor_data)
     except:
-    	my_logger.error('Received a non-UTF8 msg')
- 
-#start:    
-#make queue file folder
+        my_logger.error('Received a non-UTF8 msg')
+
+
+# start:
+# make queue file folder
 if not os.path.exists(MY_MQTT_QUEUE_FILE_PATH):
     os.makedirs(MY_MQTT_QUEUE_FILE_PATH)
-#make sending file folder    
+# make sending file folder
 if not os.path.exists(MY_SENDING_FILE_PATH):
     os.makedirs(MY_SENDING_FILE_PATH)
-#make sent file folder    
+# make sent file folder
 if not os.path.exists(MY_SENT_FILE_PATH):
     os.makedirs(MY_SENT_FILE_PATH)
-#make sending fail file folder    
+# make sending fail file folder
 if not os.path.exists(MY_SEND_FAIL_FILE_PATH):
-    os.makedirs(MY_SEND_FAIL_FILE_PATH)            
-#make log file folder
+    os.makedirs(MY_SEND_FAIL_FILE_PATH)
+# make log file folder
 if not os.path.exists(MY_LOG_FILE_PATH):
     os.makedirs(MY_LOG_FILE_PATH)
 
@@ -105,14 +108,14 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 my_logger.addHandler(handler)
 my_logger.info('I am started!')
-#example
-#my_logger.debug('debug message')
-#my_logger.info('info message')
-#my_logger.warn('warn message')
-#my_logger.error('error message')
-#my_logger.critical('critical message')
+# example
+# my_logger.debug('debug message')
+# my_logger.info('info message')
+# my_logger.warn('warn message')
+# my_logger.error('error message')
+# my_logger.critical('critical message')
 
-#clean_session=True, userdata=None, protocol=MQTTv311, transport="tcp"
+# clean_session=True, userdata=None, protocol=MQTTv311, transport="tcp"
 client = mqtt.Client(protocol=mqtt.MQTTv31)
 client.on_connect = on_connect
 client.on_message = on_message
